211c8bac974702e3a3eb012c7e35096650ab8621
tharlysc <tharlyton.scarabotto@outlook.com>
2023-06-26 14:36:20 -0300

    add: function thar creates a random number to guess

---
+++ js/randomNumber.js
@@ -0,0 +1,12 @@
+const smallerValue = 1
+const biggerValue = 1000
+const secretNumber = createRandomNumber()
+
+function createRandomNumber() {
+    return parseInt(Math.random() * biggerValue +1);
+}
+
+console.log(secretNumber);
+
+document.getElementById('smaller-value').innerHTML = smallerValue;
+document.getElementById('bigger-value').innerHTML = biggerValue;
